34c10cb9ca52f7fa0c2f11a0173360a87692d511
Sokolinyy <kostyk.vasilii@gmail.com>
2023-01-23 23:14:08 +0100

    adding grid, and overlay when hover grid elements

--- src/Main.js
+++ src/Main.js
@@ -1,166 +1,147 @@
-import React from 'react'
-import catImage from "./images/white-cat-lies-woman-s-knees.jpg"
-import { loremIpsum, LoremIpsum } from 'react-lorem-ipsum';
-import girl from "./images/work-example/girl.jpg"
-import faq from "./images/work-example/faq.jpg"
-import card from "./images/work-example/card.jpg"
-import rating from "./images/work-example/rating.jpg"
-import remote from "./images/work-example/remote.jpg"
-import suvs from "./images/work-example/suvs.jpg"
-import htmlLogo from "./images/my-skills/html-logo.png"
-import cssLogo from "./images/my-skills/css-3-logo-png-transparent.png"
-import jsLogo from "./images/my-skills/JavaScript_logo_PNG2.png"
-import reactLogo from "./images/my-skills/React-icon.svg.png"
+import React from "react";
+import catImage from "./images/white-cat-lies-woman-s-knees.jpg";
+import { loremIpsum, LoremIpsum } from "react-lorem-ipsum";
+import girl from "./images/work-example/girl.jpg";
+import faq from "./images/work-example/faq.jpg";
+import card from "./images/work-example/card.jpg";
+import rating from "./images/work-example/rating.jpg";
+import remote from "./images/work-example/remote.jpg";
+import suvs from "./images/work-example/suvs.jpg";
+import htmlLogo from "./images/my-skills/html-logo.png";
+import cssLogo from "./images/my-skills/css-3-logo-png-transparent.png";
+import jsLogo from "./images/my-skills/JavaScript_logo_PNG2.png";
+import reactLogo from "./images/my-skills/React-icon.svg.png";
+import cssHtmlJsLogo from "./images/my-skills/html-css-js-logo.png";
+import gitHubLogo from "./images/logos/GitHub-Logo.png";
 
 const Main = () => {
   return (
     <main role="main">
-      <section className='greeting'>
-        <p className='greeting--text'>
+      <section className="greeting">
+        <p className="greeting--text">
           Hello, <br /> my <br /> name <br /> is <br /> Vasilii
         </p>
 
-        <img 
-          src={catImage}
-          className='greeting--image'
-        />
+        <img src={catImage} className="greeting--image" />
       </section>
 
-      <div className='my-skills'>
-          <img src={htmlLogo}></img>
-          <img src={cssLogo}></img>
-          <img src={jsLogo}></img>
-          <img src={reactLogo}></img>
-        </div>
+      <div className="my-skills">
+        <img src={cssHtmlJsLogo}></img>
+        <img className="react-logo" src={reactLogo}></img>
+      </div>
 
-      <section 
-        className='about'
-        id='about-section'
-      >
-        <h2 className='about-me'>About Me</h2>
+      <section className="about-section" id="about-section">
+        <h2 className="about-me">About Me</h2>
         <p>
           <LoremIpsum p={1} />
         </p>
       </section>
 
-      <section 
-        className='my-works'
-        id="my-works-section"
-      >
-  
+      <section className="my-works" id="my-works-section">
         <h2>My Works</h2>
-        <h3>Short name</h3>
 
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+        <div class="grid-container">
+          <div className="grid-item">
+            <img src={girl} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={card}
-              className='stock--image'
-            />
-        </div>
 
-        <h3>Short name</h3>
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={faq}
-              className='stock--image'
-            />
-        </div>
+            </div>
+          </div>
 
-        <h3>Short name</h3>
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+          <div className="grid-item">
+            <img src={card} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={girl}
-              className='stock--image'
-            />
-        </div>
 
-        <h3>Short name</h3>
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={rating}
-              className='stock--image'
-            />
-        </div>
+            </div>
+          </div>
 
-        <h3>Short name</h3>
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+          <div className="grid-item">
+            <img src={rating} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={remote}
-              className='stock--image'
-            />
-        </div>
 
-        <h3>Short name</h3>
-        <div className='work-examples-container'>
-          <ul>
-            <li><p>Something about my work</p></li>
-            <li>
-              <a
-                className="url-to-my-work"
-                href='google.com'>GitHub link
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
               </a>
-            </li>
-          </ul>
-            <img
-              src={suvs}
-              className='stock--image'
-            />
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <img src={suvs} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
+              </a>
+
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
+              </a>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <img src={faq} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
+              </a>
+
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
+              </a>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <img src={remote} className="grid-image"></img>
+            <div className="middle">
+              <h3 className="name-of-project">Name of project</h3>
+              <a href="">
+                <button className="text">URL</button>
+              </a>
+
+              <a href="" target="_blank">
+                <button className="text">
+                  GitHub link
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
       </section>
 
-      <section
-        className='contact'
-        id="contact-section"
-      >
-
-      </section>
+      <section className="contact" id="contact-section"></section>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
